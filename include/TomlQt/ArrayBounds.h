b2e9a523a2c458ce2dd12425cc78087ef5b88748
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-07-23 23:12:42 +0200

    feat: add ArrayBounds::validate() overload for toml::array pointers
    Support validation for toml::array pointers, without requiring
    std::optional.

--- include/TomlQt/ArrayBounds.h
+++ include/TomlQt/ArrayBounds.h
@@ -9,16 +9,26 @@
 
 namespace tomlqt {
 
+// TODO: Test .validate()
 struct ArrayBounds final {
         std::optional<size_t> min_size = std::nullopt;
         std::optional<size_t> max_size = std::nullopt;
-        enum class validation_result { success, min_size_fail, max_size_fail };
+        enum class validation_result { success, min_size_fail, max_size_fail, null_ptr };
 
         [[nodiscard]] validation_result validate(const toml::array& arr) const {
                 using enum validation_result;
 
                 if (min_size && min_size > arr.size()) { return min_size_fail; }
                 if (max_size && max_size < arr.size()) { return max_size_fail; }
+                return success;
+        }
+
+        [[nodiscard]] validation_result validate(const toml::array* arr) const {
+                using enum validation_result;
+
+                if (!arr) { return null_ptr; }
+                if (min_size && min_size > arr->size()) { return min_size_fail; }
+                if (max_size && max_size < arr->size()) { return max_size_fail; }
                 return success;
         }
 };
