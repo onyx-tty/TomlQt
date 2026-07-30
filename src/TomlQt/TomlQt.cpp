b2b15176836140843ca1901ebe1a37b990644bde
onyx-tty <183672092+onyx-tty@users.noreply.github.com>
2026-07-30 21:55:17 +0200

    fix: remove redundant local template overload

--- src/TomlQt/TomlQt.cpp
+++ src/TomlQt/TomlQt.cpp
@@ -76,11 +76,6 @@ std::optional<Qt::Alignment> tomlqt::value<Qt::Alignment>(toml::node_view<const 
         return std::nullopt;
 }
 
-template<tomlqt::TomlSupported TReturn>
-[[nodiscard]] std::optional<TReturn> value(toml::node_view<const toml::node> node) {
-        return node.value<TReturn>();
-}
-
 #define INSTANTIATE(T) \
         template std::optional<T> tomlqt::value<T>(toml::node_view<const toml::node> node);
 
